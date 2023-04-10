1a63b1fe77a1adc75d762b9a58d5896d37c332ac
matsui_kouki <kottywakotty@gamil.com>
2023-04-11 00:19:04 +0900

    usercontroller追加

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+  # user画面表示
+  def show
+    @user = User.find(params[:id])
+  end
+  # user作成画面表示
+  def new
+    @user = User.new
+  end
+  # user作成処理
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to users_path
+    end
+  end
+  private
+  def user_params
+    params.require(:user).permit(:name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :tasks
   # Defines the root path route ("/")
