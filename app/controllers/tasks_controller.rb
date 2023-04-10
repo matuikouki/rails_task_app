a1b4438509b6024e9ded25fefef5d06f301514fa
matsui_kouki <kottywakotty@gamil.com>
2023-04-10 23:54:49 +0900

    事前処理をbefore_actionとして共通化

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: %i[edit update destroy]
   # Task一覧画面表示
   def index
     @tasks = Task.all
@@ -14,27 +15,26 @@ class TasksController < ApplicationController
       redirect_to tasks_path
     end
   end
-   # Task編集画面表示
   def edit
   end
   # Task更新処理
   def update
-    @task = Task.find(params[:id])
     if @task.update(task_params)
       redirect_to tasks_path
     end
   end
   # Task削除処理
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
     redirect_to tasks_path
   end
-
   private
   # ストロングパラメータ
   # permitで指定した項目を許可する
   def task_params
     params.require(:task).permit(:content)
+  end
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
